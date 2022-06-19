c906e43b198d1c764a2e450a74c9994914b4e98b
Oleg Sosnitskiy <orlovolegpr@gmail.com>
2022-06-19 12:32:43 +0300

    Added userDefinedRuntimeAttributes Set logic views and constraints

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentTrafficLightColor = CurrentTrafficLightColor.red
+    private let trafficLightsOff: CGFloat = 0.3
+    private let trafficLightOn: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.alpha = trafficLightsOff
+        yellowColorView.alpha = trafficLightsOff
+        greenColorView.alpha = trafficLightsOff
+        
+        startButton.setTitle("START", for: .normal)
     }
+    
+    @IBAction func startButtonTapped() {
 
-
+    }
+    
 }
 
+enum CurrentTrafficLightColor {
+    case red
+    case yellow
+    case green
+}
